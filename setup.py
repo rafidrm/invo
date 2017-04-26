0d90b5c145fa07588b506337e7da2351a3201cf5
Rafid Mahmood <rm@wintermute.local>
2017-04-26 17:29:01 -0400

    updated setup for pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 setup(
         name='invo',
-        version='1.0',
+        version='0.1',
         description='A Generalized Inverse Optimization package.',
         author='Rafid Mahmood',
         author_email='rafid.mahmood@mail.utoronto.ca',
+        url='https://github.com/rafidrm/invo',
+        download_url='https://github.com/rafidrm/invo/archive/0.1.tar.gz',
         license='MIT',
         packages=['invo'],
+        keywords=['optimization','inverse'],
         install_requires=['numpy', 'cvxpy'],
         zip_safe=False)
