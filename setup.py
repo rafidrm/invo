59e7e3680e840bf3bd1c6aba66f9f4694a2cc373
raynoriaga <rafid.saheb@gmail.com>
2017-10-18 15:45:32 -0400

    updating to 0.5

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@
 # By Rafid Mahmood <rafid.mahmood@mail.utoronto.ca>
 # License: MIT
 
-
 from setuptools import setup
 
-
 setup(
-        name='invo',
-        version='0.4.2',
-        description='A Generalized Inverse Optimization package.',
-        author='Rafid Mahmood',
-        author_email='rafid.mahmood@mail.utoronto.ca',
-        url='https://github.com/rafidrm/invo',
-        download_url='https://github.com/rafidrm/invo/archive/0.4.1.tar.gz',
-        license='MIT',
-        packages=['invo', 
-            'invo.LinearModels',
-            'invo.utils'],
-        keywords=['optimization','inverse'],
-        install_requires=['numpy', 'cvxpy'],
-        zip_safe=False)
+    name='invo',
+    version='0.5',
+    description='A Generalized Inverse Optimization package.',
+    author='Rafid Mahmood',
+    author_email='rafid.mahmood@mail.utoronto.ca',
+    url='https://github.com/rafidrm/invo',
+    download_url='https://github.com/rafidrm/invo/archive/0.4.1.tar.gz',
+    license='MIT',
+    packages=['invo', 'invo.LinearModels', 'invo.utils'],
+    keywords=['optimization', 'inverse'],
+    install_requires=['numpy', 'cvxpy'],
+    zip_safe=False)
